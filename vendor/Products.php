1bf2d30a768234779d890b88982d60709f52ea34
misha <mishaponomarenko11082001@gmail.com>
2020-11-12 14:07:50 +0200

    added properties and methods to Products.php

--- vendor/Products.php
+++ vendor/Products.php
@@ -4,4 +4,22 @@
 class Products
 {
 
+    public function getProduct($id)
+    {
+        $query = "SELECT * FROM products WHERE id = $id;";
+        return $this->db->query($query);
+    }
+
+    public function getCategory()
+    {
+        $category = filter_input(INPUT_GET, 'category');
+        $query = "SELECT * FROM products WHERE category LIKE '$category';";
+        $result = $this->db->query($query);
+        if ($result) {
+            while ($tmp = $result->fetch_assoc()) {
+                $this->products[] = $tmp;
+            }
+        }
+        return $this->products;
+    }
 }
