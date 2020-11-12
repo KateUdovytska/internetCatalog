45d679e4de12ed1488afbd7564960211de8935e3
misha <mishaponomarenko11082001@gmail.com>
2020-11-12 21:49:02 +0200

    added methods to Router.php

--- vendor/Router.php
+++ vendor/Router.php
@@ -3,5 +3,22 @@
 
 class Router
 {
+    static public function redirect()
+    {
+        header('Location: ' . $_SERVER['PHP_SELF']);
+    }
 
+    static public function init()
+    {
+        $controller = new Controllers();
+        $action = 'main';
+        if ($_GET['page'] === 'admin') {
+            $action = 'admin';
+        } elseif (isset($_GET['category'])) {
+            $action = 'category';
+        } elseif (isset($_GET['id'])) {
+            $action = 'item';
+        }
+        $controller->$action();
+    }
 }
