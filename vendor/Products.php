5f657bce814e7ff071fc4453ffd60cfcabf31412
misha <mishaponomarenko11082001@gmail.com>
2020-11-12 20:22:29 +0200

    fixed the getCategory method in Products.php

--- vendor/Products.php
+++ vendor/Products.php
@@ -11,6 +11,10 @@ class Products
         $this->db = new mysqli(DB_HOST, DB_USERNAME, DB_PASS, DB_NAME);
     }
 
+    /**
+     * Gets all the products from the DataBase
+     * @return array
+     */
     public function getAllProducts()
     {
         $query = "SELECT * from products;";
@@ -23,6 +27,11 @@ class Products
         return $this->products;
     }
 
+    /**
+     * Adds new product to a database and a new image to a local folder
+     * @param array $newProduct
+     * @return bool|mysqli_result
+     */
     public function addProduct(array $newProduct)
     {
         if (!file_exists(IMAGES_DIR)) {
@@ -57,6 +66,11 @@ class Products
         }
     }
 
+    /**
+     * Returns one product with the specified id
+     * @param int $id
+     * @return array|false
+     */
     public function getProduct($id)
     {
         $query = "SELECT * FROM products WHERE id = $id;";
@@ -67,9 +81,14 @@ class Products
         return false;
     }
 
+    /**
+     * Gets all the products with the specified category
+     * @param string $category
+     * @return array
+     */
     public function getCategory($category)
     {
-        $query = "SELECT * FROM products WHERE category LIKE '$category';";
+        $query = "SELECT * FROM products WHERE category_id LIKE (SELECT id FROM categories WHERE name LIKE '$category');";
         $result = $this->db->query($query);
         if ($result) {
             while ($tmp = $result->fetch_assoc()) {
@@ -79,6 +98,11 @@ class Products
         return $this->products;
     }
 
+    /**
+     * Deletes the product from the DataBase
+     * @param int $id
+     * @return bool|mysqli_result
+     */
     public function deleteProduct($id)
     {
         $query = "DELETE FROM products WHERE id = $id;";
