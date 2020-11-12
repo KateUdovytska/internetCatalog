a57bc162756cf3aeacec407a84f43bb676e58ca0
misha <mishaponomarenko11082001@gmail.com>
2020-11-12 22:36:50 +0200

    added main.php

--- views/pages/main.php
+++ views/pages/main.php
@@ -1,1 +1,30 @@
-<?php
+<div class="products">
+    <nav>
+        <h2>Categories:</h2>
+        <ul>
+            <li><a href="?category=cookies">Печенье</a></li>
+            <li><a href="?category=cakes">Торты</a></li>
+            <li><a href="?category=waffles">Вафли</a></li>
+            <li><a href="?category=chocolate">Шоколад</a></li>
+            <li><a href="?category=marshmallow">Зефир</a></li>
+        </ul>
+    </nav>
+    <?php foreach ($data as $product): ?>
+        <table class="product">
+            <tr>
+                <th><?= $product['name'] ?></th>
+            </tr>
+            <tr class="image">
+                <td>
+                    <img src="<?= IMAGES_DIR . $product['image_name'] ?>" alt="<?= $product['image_name'] ?>">
+                </td>
+            </tr>
+            <tr class="description">
+                <td><?= $product['description'] ?></td>
+            </tr>
+            <tr class="price">
+                <td><?= $product['price'] ?>$</td>
+            </tr>
+        </table>
+    <?php endforeach; ?>
+</div>
