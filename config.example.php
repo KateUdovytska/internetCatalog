14eddcfec7642c7107dd971e9806a5e108a3f96f
misha <mishaponomarenko11082001@gmail.com>
2020-11-12 13:09:14 +0200

    added examples to config.example.php

--- config.example.php
+++ config.example.php
@@ -1,5 +1,11 @@
 <?php
+// Data Base
 const DB_HOST = 'localhost';
 const DB_USERNAME = '';
 const DB_PASS = '';
 const DB_NAME = '';
+
+// Images
+const IMAGES_DIR = ''; // directory . DIRECTORY_SEPARATOR
+const MAX_IMAGE_SIZE = ''; // integer
+const AVAILABLE_IMAGE_TYPES = [];
