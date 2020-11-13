bce4fbead77976b74365f5e1dbb8e8031ec36d19
user <user@user.com>
2020-11-13 19:18:01 +0200

    "edit form in users.php"

--- views/pages/users.php
+++ views/pages/users.php
@@ -1,9 +1,9 @@
 <div class="addNewUser">
     <p>Добавить пользователя</p>
     <form method="post" class="registrationForm">
-        <input type="text" name="userLogin" placeholder="login" required>
-        <input type="password" name="userPassword" placeholder="password" required>
-        <input type="password" name="confPassword" placeholder="confirm the password" required>
+        <input type="text" name="userLogin" placeholder="login">
+        <input type="password" name="userPassword" placeholder="password">
+        <input type="password" name="confPassword" placeholder="confirm the password">
         <input type="submit" value="Add User">
     </form>
 </div>
@@ -16,23 +16,23 @@
             <th>Edit</th>
             <th>Delete</th>
         </tr>
-        <?php foreach ($data as $id => $user): ?>
-            <tr>
-                <td><?= $user['id'] ?></td>
-                <td><?= $user['login'] ?></td>
-                <td>
-                    <form method="POST">
-                        <input type="hidden" name="edit_user" value="<?= $user['id'] ?>">
-                        <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
-                    </form>
-                </td>
-                <td>
-                    <form method="POST">
-                        <input type="hidden" name="delete_user" value="<?= $user['id'] ?>">
-                        <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
-                    </form>
-                </td>
-            </tr>
+        <?php foreach ($data  as $id => $user): ?>
+        <tr>
+            <td><?=$user['id']?></td>
+            <td><?=$user['login']?></td>
+            <td>
+                <form method="POST">
+                    <input type="hidden" name="edit_user" value="<?= $user['id'] ?>">
+                    <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
+                </form>
+            </td>
+            <td>
+                <form method="POST">
+                    <input type="hidden" name="delete_user" value="<?= $user['id'] ?>">
+                    <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
+                </form>
+            </td>
+        </tr>
         <?php endforeach; ?>
     </table>
 </div>
