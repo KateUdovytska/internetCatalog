a7229695d3c513320322657bc78e6c7044616024
user <user@user.com>
2020-11-13 19:59:59 +0200

    edit table in users.php

--- views/pages/users.php
+++ views/pages/users.php
@@ -13,19 +13,12 @@
         <tr>
             <th>№</th>
             <th>User</th>
-            <th>Edit</th>
             <th>Delete</th>
         </tr>
         <?php foreach ($data  as $id => $user): ?>
         <tr>
             <td><?=$user['id']?></td>
             <td><?=$user['login']?></td>
-            <td>
-                <form method="POST">
-                    <input type="hidden" name="edit_user" value="<?= $user['id'] ?>">
-                    <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
-                </form>
-            </td>
             <td>
                 <form method="POST">
                     <input type="hidden" name="delete_user" value="<?= $user['id'] ?>">
