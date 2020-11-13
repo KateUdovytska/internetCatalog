7cf45fe3a1866acb87fbca8ad66081a9e36dd16c
kate <ketrina113@gmail.com>
2020-11-13 02:46:29 +0200

    add link

--- views/templates/default.php
+++ views/templates/default.php
@@ -5,8 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title><?= $this->page ?></title>
+
 </head>
 <body>
 <header>
