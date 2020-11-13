fe0d34a35846b67a3c5f6b28f42a3ca790d7ae19
user <user@user.com>
2020-11-13 19:10:51 +0200

    add forms in users.php

--- views/pages/users.php
+++ views/pages/users.php
@@ -1,1 +1,38 @@
-<?php
+<div class="addNewUser">
+    <p>Добавить пользователя</p>
+    <form method="post" class="registrationForm">
+        <input type="text" name="userLogin" placeholder="login">
+        <input type="password" name="userPassword" placeholder="password">
+        <input type="password" name="confPassword" placeholder="confirm the password">
+        <input type="submit" value="Add User">
+    </form>
+</div>
+
+<div class="userList">
+    <table>
+        <tr>
+            <th>№</th>
+            <th>User</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        <?php foreach ($data  as $id => $user): ?>
+        <tr>
+            <td><?=$user['id']?></td>
+            <td><?=$user['login']?></td>
+            <td>
+                <form method="POST">
+                    <input type="hidden" name="edit_user" value="<?= $user['id'] ?>">
+                    <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
+                </form>
+            </td>
+            <td>
+                <form method="POST">
+                    <input type="hidden" name="delete_user" value="<?= $user['id'] ?>">
+                    <button type="submit" value="del" class="delete"><i class="far fa-trash-alt"></i></button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
