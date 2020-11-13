89c2d5c329d8f1b1a67a0cb1cd22bee18c7d6f63
Someone <rgbjkbs@hdbf.ru>
2020-11-13 20:37:36 +0200

    was changed main.php

--- views/pages/main.php
+++ views/pages/main.php
@@ -1,24 +1,4 @@
 <?php session_unset(); ?>
-<nav>
-    <h2>Categories:</h2>
-    <ul>
-        <li>
-            <a href="?category=cookies">Печенье</a>
-        </li>
-        <li>
-            <a href="?category=cakes">Торты</a>
-        </li>
-        <li>
-            <a href="?category=waffles">Вафли</a>
-        </li>
-        <li>
-            <a href="?category=chocolate">Шоколад</a>
-        </li>
-        <li>
-            <a href="?category=marshmallow">Зефир</a>
-        </li>
-    </ul>
-</nav>
 <div class="products-description">
     <table class="table_products">
         <caption>All Products</caption>
